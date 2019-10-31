e6ed5ffe3a819c40b61c8b49ac3ac7e917b2b86d
akshat157 <ancientfuture1337@gmail.com>
2019-10-31 18:12:36 +0530

    Change URI env var

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-	'url' : `mongodb://${process.env.database_username}:${process.env.database_password}@${process.env.database_address}/${process.env.database_name}`
+	// 'url' : `mongodb://${process.env.database_username}:${process.env.database_password}@${process.env.database_address}/${process.env.database_name}`
+	'url' : `${ process.env.DATABASE_URI }`
 }
